2a35c7d34be5a7bddbad23f626f04e6829da34c6
faris <farissoehartono@gmail.com>
2025-05-22 15:57:23 +0700

    adding main, db connection, and server structure

---
+++ main.js
@@ -0,0 +1,52 @@
+const { pool, checkDB } = require('./database/db.js');
+const { authUser, authLembaga, authAdmin } = require('./auth/auth.js');
+const {
+  rootRouter,
+  loginUserRouter,
+  registerUserRouter,
+  loginLembagaRouter,
+  registerLembagaRouter,
+  loginAdminRouter,
+  registerAdminRouter,
+  userRouter,
+  lembagaRouter,
+  adminRouter,
+} = require('./routes/mainRouter.js');
+
+const express = require('express');
+const session = require('express-session');
+
+checkDB();
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    }
+  })
+);
+
+app.use("/", rootRouter);
+
+app.use('/login/user', loginUserRouter);
+app.use('/register/user', registerUserRouter);
+app.use('/login/lembaga', loginLembagaRouter);
+app.use('/register/lembaga', registerLembagaRouter);
+app.use('/login/admin', loginAdminRouter);
+app.use('/register/admin', registerAdminRouter);
+
+app.use('/admin', authAdmin, adminRouter);
+app.use('/lembaga', authLembaga, lembagaRouter);
+app.use('/user', authUser, userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
