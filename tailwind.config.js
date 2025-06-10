bbab7193724078a006c0ad8a40e2364261cefdbc
!rez <dimasrezaa117@gmail.com>
2025-06-10 13:15:47 +0700

    resolve css problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./private/**/*.html"],
+  content: [
+    "./private/**/*.html",
+    "./private/**/*.js"
+  ],
   theme: {
     extend: {},
   },
