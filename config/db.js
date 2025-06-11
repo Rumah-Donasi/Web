ddd772e570487b8d684a6a88faa6162b3c2f597b
Elight-dotcom <ardanuash@gmail.com>
2025-06-11 18:15:41 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg'); // ← WAJIB ADA
 
-const db = new Pool({
+const pool = new Pool({
   host: 'db.ocuwghtrsjrfqnqyfvjn.supabase.co',
   port: 5432,
   user: 'postgres',
   password: process.env.DB_PASSWORD,
   database: 'postgres',
   ssl: {
-    rejectUnauthorized: false, // Wajib untuk koneksi ke Supabase
+    rejectUnauthorized: false,
   },
 });
 
