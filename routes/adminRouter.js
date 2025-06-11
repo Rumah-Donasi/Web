6d3065587239ab1d83f9f35bd4a1a2b6cc2d6935
Elight-dotcom <ardanuash@gmail.com>
2025-06-11 21:36:33 +0700

    Add files via upload

---
+++ routes/adminRouter.js
@@ -0,0 +1,31 @@
+const path=require('path');
+const express = require('express');
+const { 
+  getVerifikasi,
+  getHistory,
+  getIssue, 
+  admlogout,
+  putVerifikasi,
+  putIssue,
+  deleteVerifikasi,
+  deleteHistory,
+  deleteIssue
+} = require('../controllers/adminController');
+const { adminLogin } = require('../controllers/loginController');
+const router = express.Router();
+
+//redirect /admin to admintool.html
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../private/admintool.html'));
+});
+
+router.get('/verifikasi', getVerifikasi);
+router.get('/history', getHistory);
+router.get('/issue', getIssue);
+router.get('/logout',admlogout);
+router.put('/updateVerifikasi', putVerifikasi);
+router.put('/updateIssue', putIssue);
+router.delete("/deleteVerifikasi/:id_lembaga",deleteVerifikasi);
+router.delete("/deleteHistory/:id_detail",deleteHistory)
+router.delete("/deleteIssue/:id_issue",deleteIssue)
+module.exports = router;
